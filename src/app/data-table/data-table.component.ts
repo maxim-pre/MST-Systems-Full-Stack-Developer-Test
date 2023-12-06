e7961c048b9ce98e78ed3235c9c3224865f537e0
Maxim Prestwich <maximprestwich1@gmail.com>
2023-12-06 14:32:56 +0000

    establish connection to socket.io server & verify recieved data

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { SocketService } from '../services/socket.service';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
-  styleUrls: ['./data-table.component.css']
+  styleUrls: ['./data-table.component.css'],
 })
 export class DataTableComponent {
+  constructor(private socketService: SocketService) {}
 
+  ngOnInit() {
+    this.socketService.onDataUpdate().subscribe((data: any) => {
+      console.log(data);
+    });
+  }
 }
--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { io } from 'socket.io-client';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SocketService {
+  private socket: any;
 
-  constructor() { }
+  constructor() {
+    // establish two-way connection with socket.io server
+    this.socket = io('https://mst-full-stack-dev-test.herokuapp.com/');
+  }
+
+  onDataUpdate(): Observable<any> {
+    return new Observable((observer) => {
+      // listen for the 'data-update' event
+      this.socket.on('data-update', (data: any) => {
+        observer.next(data);
+      });
+    });
+  }
 }
