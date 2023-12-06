ec23761f840f50e8e80f2e773f88f5254c003df6
Maxim Prestwich <maximprestwich1@gmail.com>
2023-12-06 15:03:22 +0000

    create onSort function in data-table component

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { SocketService } from '../services/socket.service';
+import { SortingService } from '../services/sorting.service';
+import { sortColumn } from '../sortColumn';
 
 @Component({
   selector: 'app-data-table',
@@ -7,8 +9,23 @@ import { SocketService } from '../services/socket.service';
   styleUrls: ['./data-table.component.css'],
 })
 export class DataTableComponent {
-  constructor(private socketService: SocketService) {}
   data: any[] = [];
+  sortColumn: sortColumn = { key: 'MSTID', asc: true };
+
+  constructor(
+    private socketService: SocketService,
+    private sortingService: SortingService
+  ) {}
+
+  onSort(key: string) {
+    if (key === this.sortColumn.key) {
+      this.sortColumn = { key: key, asc: !this.sortColumn.asc };
+    } else {
+      this.sortColumn = { key: key, asc: true };
+    }
+
+    this.data = this.sortingService.sortData(this.data, this.sortColumn);
+  }
 
   ngOnInit() {
     this.socketService.onDataUpdate().subscribe((data: any) => {
