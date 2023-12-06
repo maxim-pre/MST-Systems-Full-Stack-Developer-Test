b4ad3aeb981a1fb905a2f6056ecde63623488b81
Maxim Prestwich <maximprestwich1@gmail.com>
2023-12-06 14:57:24 +0000

    create sortData function in sorting.service.ts

--- src/app/services/sorting.service.ts
+++ src/app/services/sorting.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
+import { sortColumn } from '../sortColumn';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SortingService {
+  constructor() {}
 
-  constructor() { }
+  sortData(data: any[], sortColumn: sortColumn) {
+    const sortedData = _.orderBy(
+      data,
+      [sortColumn.key],
+      [sortColumn.asc === true ? 'asc' : 'desc']
+    );
+
+    return sortedData;
+  }
 }
